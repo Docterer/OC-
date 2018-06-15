58185864d55bace0f3c6662c87e9fdf74dfd99fa
docterer <daladidali@gmail.com>
2018-06-15 09:03:37 +0800

    公有属性和私有属性添加位置的注意事项

--- iOS学习笔记/ViewController.h
+++ iOS学习笔记/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+/***在.h文件中添加的属性为暴露的公有属性，这样就是在其他的类中只需要实例化了ViewController对象，就能改变暴露的类属性的值,在Controller之间传值可以使用这种方法***/
+@property(nonatomic,weak)UIImage *image;
 
 @end
 
--- iOS学习笔记/ViewController.m
+++ iOS学习笔记/ViewController.m
@@ -9,13 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+/**在.m文件里的interface中添加该类的私有属性,   .m文件的interface称为类扩展**/
+@property(nonatomic,weak)UILabel *label;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self clickRedButton];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,6 +25,11 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma -mark 添加点击方法
+-(void)clickRedButton{
+    NSLog(@"%s",__func__);
 }
 
 
